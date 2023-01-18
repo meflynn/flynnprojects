4501429b24f8779235daf99bb7b891de71bebf30
Michael Flynn <flynn.michael.e@gmail.com>
2023-01-18 15:58:35 -0600

    Update caption top margin.

--- R/theme-flynn.R
+++ R/theme-flynn.R
@@ -14,7 +14,7 @@ theme_flynn <- function(base_size = 11, base_family = ""){
 
     theme(plot.title = element_text(face = "bold", size = base_size*1.4, hjust = 0, margin = margin(t = 0, b = 0.2, l = 0, r = 0, unit = "cm")),
           plot.subtitle = element_text(size = base_size * 1.1, hjust = 0, margin = margin(b = 0.1, unit = "cm")),
-          plot.caption = element_text(face = "italic", size = base_size * 0.7, hjust = 1),
+          plot.caption = element_text(face = "italic", size = base_size * 0.7, hjust = 1, margin = margin(t = 0.2, unit = "cm")),
           panel.border = element_rect(fill = NA, size = 0.2),
           strip.background = element_rect(fill = "gray80", color = "black", size = 0.2),
           strip.text = element_text(size = base_size, color = "black", face = "bold", margin = margin(t = 0.2, b = 0.2, l = 0.2, r = 0.2, unit = "cm")),
