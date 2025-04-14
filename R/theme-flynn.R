7801c82a867b27b2b21bf1349b154c2eaeb3d069
Michael Flynn <flynn.michael.e@gmail.com>
2025-04-14 10:51:23 -0500

    Update figure theme to remove vertical lines. Let's try this out and see how it looks.

--- R/theme-flynn.R
+++ R/theme-flynn.R
@@ -19,13 +19,15 @@ theme_flynn <- function(base_size = 11, base_family = ""){
           strip.background = element_rect(fill = "gray80", color = "black", size = 0.2),
           strip.text = element_text(size = base_size, color = "black", face = "bold", margin = margin(t = 0.2, b = 0.2, l = 0.2, r = 0.2, unit = "cm")),
           panel.background = element_rect(size = 0.2),
-          panel.grid.major = element_line(color = "gray70", size = 0.15),
-          panel.grid.minor = element_line(color = "gray90", size = 0.1),
+          panel.grid.major.y = element_line(color = "gray70", size = 0.15),
+          panel.grid.minor.y = element_blank(),
+          panel.grid.major.x = element_blank(),
+          panel.brid.minor.x = element_blank(),
           axis.title = element_text(face = "bold", size = base_size),
           axis.title.y = element_text(angle = 90, margin = margin(t = 0, r = 0.5, b = 0, l = 0, unit = "cm")),
           axis.title.x = element_text(margin = margin(t = 0.5, r = 0, b = 0.3, l = 0, unit = "cm")),
           axis.ticks = element_line(size = 0.1),
           axis.ticks.length = unit(0.1, "cm"),
-          legend.title = element_text(size = base_size * 1.1, face = "bold", hjust = 0, margin = margin(t = 0, b = 0.20, l = 0, r = 0, unit = "cm")),
+          legend.title = element_text(size = base_size * 1.1, face = "bold", hjust = 0, margin = margin(t = 0, b = 0.10, l = 0, r = 0, unit = "cm")),
           plot.margin = margin(0.5, 0.5, 0.5, 0.5, unit = "cm"))
 }
